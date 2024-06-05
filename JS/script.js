e8b4b59fa0f4df732d743b2bf6ac1021af112eb4
Humaira Islam <ihumaira.webfront@gmail.com>
2024-06-05 21:19:17 +0600

    Added send money function

--- JS/script.js
+++ JS/script.js
@@ -12,13 +12,15 @@ let homepage=document.querySelector('.homepage')
 // homepage variables start
 
 let balanceAmt=document.querySelector('.balanceAmt') //shown account balance
-let rechAmt=document.querySelector('#rechAmt') //recharge account 
-let recPhone=document.querySelector('#pNumb')//recipient's phone number
-let amount=document.querySelector('#amount') //amount to send
+
+let rechAmt=document.querySelector('#rechAmt') //recharge account with this amount
 let rechargeButton=document.querySelector('.rechargeButton')
 let error2=document.querySelector('.error2')
 
-
+let sendButton=document.querySelector('.sendButton')
+let amount=document.querySelector('#amount') //amount to send
+let recPhone=document.querySelector('#pNumb')//recipient's phone number
+let error3=document.querySelector('.error3')
 // homepage variables end
 
 
@@ -67,10 +69,36 @@ loginButton.addEventListener('click',()=>{
 // ================RECHARGE ACCOUNT END===================//
 
 // ================SEND MONEY START===================//
+sendButton.addEventListener('click',()=>{
+    let balanceID=document.getElementById('balanceID').textContent
+    let balanceNumb=parseInt(balanceID)
+    let recPhoneString=recPhone.value.toString()
+    // console.log(recPhoneString)
+    let amtNumb=parseInt(document.getElementById('amount').value)
+    if (amount.value=='' || recPhone.value==''){
+        error3.innerHTML='None of the fields can be empty'
+    }
+    else if (recPhoneString.length<11){
+        error3.innerHTML='Please provide a proper phone number with 11 digits'
+    }
+    else if(balanceNumb==0){
+        error3.innerHTML='Your current account balance is 0. Please recharge your account to send money.'
+    }
+    else if(balanceNumb<amtNumb){
+        error3.innerHTML='Insufficient balance'
+    }
+    
+    else{
+        error3.innerHTML=''
+        let totalBalance=balanceNumb-amtNumb
+        // console.log(totalBalance)
+        balanceAmt.innerHTML=totalBalance
+    }
+    
+})
 
 
-
-// ================SEND MONEY END===================//
+// ===================SEND MONEY END===================//
 
 
 
